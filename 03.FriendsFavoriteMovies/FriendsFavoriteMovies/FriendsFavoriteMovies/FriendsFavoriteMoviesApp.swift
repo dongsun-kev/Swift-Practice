9fc98a317ec3534cfed2d240c0db3d6a2af7c1a3
dongsun-kev <dongsun3658@gmail.com>
2026-04-15 00:38:01 +0900

    Day4 완료

---
+++ 03.FriendsFavoriteMovies/FriendsFavoriteMovies/FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by DS on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
