8042856a9c42e6b157d0ec088e495bf1d7bc0b6e
dongsun-kev <dongsun3658@gmail.com>
2026-04-19 21:18:09 +0900

    Day7 완료

---
+++ 07. Complete a game with logic/Alphabetizer/Alphabetizer/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Alphabetizer
+//
+//  Created by DS on 4/19/26.
+//
+import Foundation
+
+enum Message : String {
+    case instructions = "Place the tiles in a alphabetical order"
+    case tryAgain = "Almost 😅"
+    case youWin = "You win! 🏆"
+}
